84e8c8215feb2df767be2b4a516f4a27c70cb075
kashchei94 <kishchak.maksym1@gmail.com>
2024-05-28 13:11:08 +0300

    add register/login-page

--- FrontEnd/AngularApp/src/app/app.component.ts
+++ FrontEnd/AngularApp/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {RegisterComponent} from "./register/register.component";
+import {LogInComponent} from "./log-in/log-in.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HeaderComponent , RegisterComponent , LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AngularApp';
+  protected readonly HeaderComponent = HeaderComponent;
 }
