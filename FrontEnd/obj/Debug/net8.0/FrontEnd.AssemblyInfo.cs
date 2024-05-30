17e2c6cefb53acf61aec6acff94eefaf67e9a146
kashchei94 <kishchak.maksym1@gmail.com>
2024-05-30 13:40:55 +0300

    fix

--- FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011fffbdc4b862e8590aa2101dc9278a0cbee073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d44340a469354377e372d84f811c2f96e5afa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
